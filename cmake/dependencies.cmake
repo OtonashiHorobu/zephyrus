c7d8a4f27b144804da86c30b69e71d3f9ebf4644
OtonashiHorobu <3164432713@qq.com>
2026-06-30 00:18:20 +0800

    build: fix full static build

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -14,51 +14,76 @@ function(setup_dependencies)
 
     CPMAddPackage(
         URI "gh:madler/zlib@1.3.2#da607da739fa6047df13e66a2af6b8bec7c2a498"
+        OVERRIDE_FIND_PACKAGE
         OPTIONS
             "ZLIB_BUILD_TESTING OFF"
             "ZLIB_BUILD_SHARED OFF"
     )
-    set(ZLIB_ROOT "${PROJECT_BINARY_DIR}/_deps/zlib-src")
-    if(zlib_ADDED)
-        if(NOT TARGET ZLIB::ZLIB)
-            add_library(ZLIB::ZLIB ALIAS zlibstatic)
-        endif()
+    add_library(ZLIB::ZLIB ALIAS zlibstatic)
+
+    CPMAddPackage(
+        URI "https://gitlab.com/bzip2/bzip2.git@1.0.8#6a8690fc8d26c815e798c588f796eabe9d684cf0"
+        OVERRIDE_FIND_PACKAGE
+    )
+    add_library(bzip2 STATIC
+        "${bzip2_SOURCE_DIR}/blocksort.c"
+        "${bzip2_SOURCE_DIR}/huffman.c"
+        "${bzip2_SOURCE_DIR}/crctable.c"
+        "${bzip2_SOURCE_DIR}/randtable.c"
+        "${bzip2_SOURCE_DIR}/compress.c"
+        "${bzip2_SOURCE_DIR}/decompress.c"
+        "${bzip2_SOURCE_DIR}/bzlib.c"
+    )
+    target_include_directories(bzip2 PUBLIC "${bzip2_SOURCE_DIR}")
+    if(MSVC)
+        target_compile_definitions(bzip2 PRIVATE _CRT_SECURE_NO_WARNINGS)
     endif()
+    add_library(BZip2::BZip2 ALIAS bzip2)
+
+    CPMAddPackage(
+        URI "gh:google/brotli@1.2.0#028fb5a23661f123017c060daa546b55cf4bde29"
+        OVERRIDE_FIND_PACKAGE
+        OPTIONS
+            "BROTLI_DISABLE_TESTS ON"
+    )
+    set(BROTLIDEC_LIBRARIES brotlidec CACHE INTERNAL "Force freetype to use our brotlidec" FORCE)
+    set(BROTLIDEC_INCLUDE_DIRS "${PROJECT_BINARY_DIR}/_deps/brotli-src/c/include" CACHE INTERNAL "Force freetype to use our brotlidec" FORCE)
 
     CPMAddPackage(
         URI "gh:pnggroup/libpng@1.8.0#d1d0abeffede1cc898ddc3d0e600839cf026d749"
+        OVERRIDE_FIND_PACKAGE
         OPTIONS
             "PNG_TESTS OFF"
             "PNG_TOOLS OFF"
             "PNG_SHARED OFF"
+            "SKIP_INSTALL_ALL ON"
     )
-    set(PNG_ROOT "${PROJECT_BINARY_DIR}/_deps/libpng-src")
-    if(png_ADDED)
-        if(NOT TARGET PNG::PNG)
-            add_library(PNG::PNG ALIAS png_static)
-        endif()
-    endif()
+    set(PNG_LIBRARY "${PROJECT_BINARY_DIR}/_deps/libpng-src")
+    set(PNG_PNG_INCLUDE_DIR "${PROJECT_BINARY_DIR}/_deps/libpng-src")
+    add_library(PNG::PNG ALIAS png_static)
 
-    CPMAddPackage("https://gitlab.com/bzip2/bzip2.git@1.0.8#6a8690fc8d26c815e798c588f796eabe9d684cf0")
-    set(BZip2_ROOT "${PROJECT_BINARY_DIR}/_deps/bzip2-src")
-
-    CPMAddPackage("gh:harfbuzz/harfbuzz@14.2.1#56feae4035bdd48f62ba2b8d8c16232d4d89b3a4")
-    set(HarfBuzz_ROOT "${PROJECT_BINARY_DIR}/_deps/harfbuzz-src")
-    if(harfbuzz_ADDED)
-        if(NOT TARGET HarfBuzz::HarfBuzz)
-            add_library(HarfBuzz::HarfBuzz ALIAS harfbuzz)
-        endif()
-    endif()
+    CPMAddPackage(
+        URI "gh:harfbuzz/harfbuzz@14.2.1#56feae4035bdd48f62ba2b8d8c16232d4d89b3a4"
+        OVERRIDE_FIND_PACKAGE
+        OPTIONS
+            "HB_HAVE_FREETYPE OFF"
+            "SKIP_INSTALL_ALL ON"
+    )
+    add_library(HarfBuzz::HarfBuzz ALIAS harfbuzz)
 
     CPMAddPackage(
         URI "gh:freetype/freetype@2.14.3#0a0221a1347e2f1e07c395263540026e9a0aa7c7"
-        OPTIONS "FT_DYNAMIC_HARFBUZZ FALSE"
+        OVERRIDE_FIND_PACKAGE
+        OPTIONS
+            "FT_DYNAMIC_HARFBUZZ FALSE"
+            "FT_REQUIRE_ZLIB ON"
+            "FT_REQUIRE_PNG ON"
+            "FT_REQUIRE_BZIP2 ON"
+            "FT_REQUIRE_HARFBUZZ ON"
+            "FT_REQUIRE_BROTLI ON"
+            "SKIP_INSTALL_ALL ON"
     )
-    if(freetype_ADDED)
-        if(NOT TARGET Freetype::Freetype)
-            add_library(Freetype::Freetype ALIAS freetype)
-        endif()
-    endif()
+    add_library(Freetype::Freetype ALIAS freetype)
     CPMAddPackage(
         URI "gh:mikke89/RmlUi@6.2#2230d1a6e8e0848ed87a5761e2a5160b2a175ba4"
     )
